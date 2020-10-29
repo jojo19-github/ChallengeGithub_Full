6a1facb2dd48def3632ba53ad5e45770fe9a9ceb
Josiane Mendy <mendyjosiane19@gmail.com>
2020-10-29 15:53:40 +0000

    modification indexhtml/js script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 var poeme = document.getElementById('poeme').textContent;
 var compteur = document.getElementById('compteur');
 compteur.innerHTML = poeme.length;
+
+var legumes = [];
+legumes[0]= "choux";
+legumes[1]="gombo";
+legumes[2]="aubergine";
+legumes[3]="patate douce";
+legumes[4]="pomme de terre";
+legumes[5]="carotte";
+legumes[6]="brocoli";
+legumes[7]="haricot vert";
+legumes[8]="tomate";
+legumes[9]="chou fleur";
+legumes[10]="tubercule";
+
+console.log(legumes[0]);
+console.log(legumes[10]);
+console.log(document.querySelector("compteur")>3000);
